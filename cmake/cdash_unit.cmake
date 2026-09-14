89bf687feb5db1ab75c39aecced9f4c829334eab
Brandon Odiwuor <brandon.odiwuor@gmail.com>
2026-09-14 09:43:39 +0300

    Add experimental CDash client for Core unit, nowallet, and ASan profiles

---
+++ cmake/cdash_unit.cmake
@@ -0,0 +1,151 @@
+cmake_minimum_required(VERSION 3.25)
+
+if(NOT CTEST_SOURCE_DIRECTORY)
+  set(CTEST_SOURCE_DIRECTORY "$ENV{BITCOIN_SOURCE_DIR}")
+endif()
+get_filename_component(CTEST_SOURCE_DIRECTORY "${CTEST_SOURCE_DIRECTORY}" ABSOLUTE)
+
+if(NOT EXISTS "${CTEST_SOURCE_DIRECTORY}/CMakeLists.txt")
+  message(FATAL_ERROR "Set BITCOIN_SOURCE_DIR to a Bitcoin Core tree")
+endif()
+
+if(NOT CDASH_PROFILE)
+  set(CDASH_PROFILE "unit")
+endif()
+if(NOT CDASH_MODEL)
+  set(CDASH_MODEL "Experimental")
+endif()
+if(NOT DEFINED CDASH_SUBMIT)
+  set(CDASH_SUBMIT ON)
+endif()
+
+set(_profile_ok FALSE)
+foreach(_p unit unit-nowallet unit-asan)
+  if(CDASH_PROFILE STREQUAL _p)
+    set(_profile_ok TRUE)
+  endif()
+endforeach()
+if(NOT _profile_ok)
+  message(FATAL_ERROR "CDASH_PROFILE must be unit, unit-nowallet, or unit-asan")
+endif()
+
+if(NOT CDASH_MODEL STREQUAL "Experimental" AND NOT CDASH_MODEL STREQUAL "Nightly")
+  message(FATAL_ERROR "CDASH_MODEL must be Experimental or Nightly")
+endif()
+
+set(CTEST_BINARY_DIRECTORY "${CTEST_SOURCE_DIRECTORY}/build-${CDASH_PROFILE}")
+set(CTEST_CMAKE_GENERATOR "Ninja")
+set(CTEST_USE_LAUNCHERS ON)
+set(CTEST_UPDATE_VERSION_ONLY ON)
+find_program(CTEST_UPDATE_COMMAND git)
+site_name(CTEST_SITE)
+
+execute_process(
+  COMMAND git -C "${CTEST_SOURCE_DIRECTORY}" describe --tags --always --dirty
+  OUTPUT_VARIABLE _git_desc OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+if(NOT _git_desc)
+  set(_git_desc "unknown")
+endif()
+set(CTEST_BUILD_NAME "${CDASH_PROFILE}-${CMAKE_HOST_SYSTEM_NAME}-${_git_desc}")
+
+set(_submit_url "${CTEST_SUBMIT_URL}")
+if(NOT _submit_url)
+  set(_submit_url "http://127.0.0.1:8080/submit.php?project=core")
+endif()
+
+include(ProcessorCount)
+ProcessorCount(N)
+if(N EQUAL 0)
+  set(N 1)
+endif()
+
+function(cdash_submit_part part)
+  if(NOT CDASH_SUBMIT)
+    return()
+  endif()
+  ctest_submit(PARTS ${part} RETRY_COUNT 3 RETRY_DELAY 5)
+endfunction()
+
+get_filename_component(_launchers "${CMAKE_CURRENT_LIST_DIR}/enable_launchers.cmake" ABSOLUTE)
+
+set(_opts
+  "-DCMAKE_BUILD_TYPE=RelWithDebInfo"
+  "-DBUILD_TESTS=ON"
+  "-DBUILD_GUI=OFF"
+  "-DBUILD_GUI_TESTS=OFF"
+  "-DBUILD_BENCH=OFF"
+  "-DBUILD_FUZZ_BINARY=OFF"
+  "-DCTEST_USE_LAUNCHERS=ON"
+  "-DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=${_launchers}"
+)
+
+if(CDASH_PROFILE STREQUAL "unit")
+  list(APPEND _opts "-DENABLE_WALLET=ON")
+elseif(CDASH_PROFILE STREQUAL "unit-nowallet")
+  list(APPEND _opts "-DENABLE_WALLET=OFF")
+elseif(CDASH_PROFILE STREQUAL "unit-asan")
+  list(APPEND _opts
+    "-DENABLE_WALLET=ON"
+    "-DCMAKE_C_COMPILER=clang"
+    "-DCMAKE_CXX_COMPILER=clang++"
+    "-DSANITIZERS=address,undefined"
+  )
+endif()
+
+set(CTEST_CONFIGURE_COMMAND "${CMAKE_COMMAND} -S ${CTEST_SOURCE_DIRECTORY} -B ${CTEST_BINARY_DIRECTORY} -G Ninja")
+foreach(_o IN LISTS _opts)
+  string(APPEND CTEST_CONFIGURE_COMMAND " ${_o}")
+endforeach()
+
+message(STATUS "Model  : ${CDASH_MODEL}")
+message(STATUS "Profile: ${CDASH_PROFILE}")
+message(STATUS "Source : ${CTEST_SOURCE_DIRECTORY}")
+message(STATUS "Binary : ${CTEST_BINARY_DIRECTORY}")
+message(STATUS "Name   : ${CTEST_BUILD_NAME}")
+message(STATUS "Submit : ${_submit_url}")
+
+file(MAKE_DIRECTORY "${CTEST_BINARY_DIRECTORY}")
+
+ctest_start(${CDASH_MODEL})
+set(CTEST_SUBMIT_URL "${_submit_url}")
+cdash_submit_part(Start)
+
+if(CTEST_UPDATE_COMMAND)
+  ctest_update()
+  cdash_submit_part(Update)
+endif()
+
+ctest_configure(RETURN_VALUE configure_result)
+cdash_submit_part(Configure)
+
+ctest_build(
+  PARALLEL_LEVEL 3
+  NUMBER_ERRORS build_errors
+  NUMBER_WARNINGS build_warnings
+  RETURN_VALUE build_result
+)
+cdash_submit_part(Build)
+
+if(CDASH_PROFILE STREQUAL "unit-asan")
+  set(CTEST_MEMORYCHECK_TYPE "AddressSanitizer")
+  set(CTEST_MEMORYCHECK_SANITIZER_OPTIONS "detect_leaks=1:abort_on_error=1")
+  ctest_memcheck(PARALLEL_LEVEL ${N} RETURN_VALUE test_result)
+  cdash_submit_part(MemCheck)
+else()
+  ctest_test(PARALLEL_LEVEL 3 RETURN_VALUE test_result)
+  cdash_submit_part(Test)
+endif()
+
+cdash_submit_part(Done)
+
+if(NOT CDASH_SUBMIT)
+  message(STATUS "Skipping submit")
+endif()
+
+message(STATUS "configure=${configure_result} build=${build_result} "
+               "errors=${build_errors} warnings=${build_warnings} "
+               "test=${test_result}")
+
+if(configure_result OR build_result OR test_result)
+  message(FATAL_ERROR "Dashboard failed")
+endif()
---
+++ cmake/enable_launchers.cmake
@@ -0,0 +1,2 @@
+set(CTEST_USE_LAUNCHERS ON CACHE BOOL "Use CTest launchers" FORCE)
+include(CTestUseLaunchers)
